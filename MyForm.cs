a8a39edc3daaec654abab94033b48ef93ac16717
Jorge Euceda <jeuce004@fiu.edu>
2018-08-28 14:23:07 -0400

    created basic form class with constructor

---
+++ MyForm.cs
@@ -0,0 +1,15 @@
+using System;
+
+/*
+    The main Form class
+    contains the constructor and all methods needed
+    for our form
+*/
+class MyForm : Form
+{
+    public MyForm(string title, string name)
+    {
+        this.title = title;
+        this.name = name;
+    }
+}
