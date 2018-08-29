8279dd2591c09cc5482511f60cc7000e5f75a843
Emmanuel500 <32080181+Emmanuel500@users.noreply.github.com>
2018-08-28 22:17:01 -0400

    Add files via upload

---
+++ FirstForm.cs
@@ -0,0 +1,25 @@
+//Homework 1 Form
+using System.Windows.Forms;
+using System.Drawing;
+
+class FirstForm: Form {
+	
+	
+	
+	//Input Variables
+	string formTitle;
+	string userName;
+	
+	//Component Variables
+	
+	
+	//Constructor
+	public FirstForm(string formTitle, string userName) {
+		this.formTitle = formTitle;
+		this.userName = userName;
+		
+	}
+	
+	
+	
+}
