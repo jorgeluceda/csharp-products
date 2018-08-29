7e56650eb8ae04e044a1a9caa1deae1de80c4951
Emmanuel500 <32080181+Emmanuel500@users.noreply.github.com>
2018-08-28 23:13:17 -0400

    Add files via upload

---
+++ FirstForm.cs
@@ -0,0 +1,35 @@
+//0Homework 1 Form
+using System.Windows.Forms;
+using System.Drawing;
+
+class FirstForm: Form {
+	
+	//Input Variable
+	string userName;	
+
+	//Panels
+	Panel panel1;
+	Panel panel2;
+	
+	//Constructor
+	public FirstForm(string formTitle, string userName) {
+		this.Text = formTitle;
+		this.userName = userName;
+        	this.Size = new Size(500, 500);
+		this.panelOne();
+		this.panelTwo();
+	}
+	
+	//Setting up Panel 1
+	private void panelOne() {
+		panel1 = new Panel();
+		panel1.
+		this.Controls.Add(panel1);
+	}
+	
+	//Setting up Panel 2
+	private void panelTwo() {
+		
+	}
+	
+}
