abafc7c0f977e0de1f84e82b5faa2c84a81187d9
Lazaro Alvarez <lalvarezcu@yahoo.es>
2018-08-28 20:39:33 -0700

    Update default.cs

--- default.cs
+++ default.cs
@@ -35,6 +35,8 @@ namespace WindowsFormProkect
     {
 
         private String name;
+        private TextBox textbox;
+        private List<Control> ControlList;
 
         public ExtendedForm(String title, String name)
         {
@@ -43,38 +45,66 @@ namespace WindowsFormProkect
             this.name = name;
             this.Size = new Size(500, 250);
 
-            Panel panel = new Panel();
-            panel.BackColor = Color.Red;
-            panel.Dock = System.Windows.Forms.DockStyle.Bottom;
-            panel.Location = new Point(0, 0);
+            Panel panelOne = new Panel();
+            panelOne.BackColor = Color.Red;
+            panelOne.Dock = System.Windows.Forms.DockStyle.Bottom;
+            panelOne.Location = new Point(0, 0);
 
             Label label = new Label();
             label.Text = this.name;
             label.Top = 15;
             label.Left = 15;
 
-            panel.Controls.Add(label);
+            panelOne.Controls.Add(label);
 
-            TextBox textbox = new TextBox();
+            Panel panelTwo = new Panel();
+            panelTwo.BackColor = Color.Red;
+            panelTwo.Dock = System.Windows.Forms.DockStyle.Bottom;
+            panelTwo.Location = new Point(0, 0);
+
+            Label labelTwo = new Label();
+            labelTwo.Text = "Comment";
+            labelTwo.Top = 15;
+            labelTwo.Left = 15;
+
+            panelTwo.Controls.Add(labelTwo);
+
+            textbox = new TextBox();
             textbox.Anchor = (AnchorStyles.Top | AnchorStyles.Left);
 
-            panel.Controls.Add(textbox);
+            panelTwo.Controls.Add(textbox);
 
             Button button = new Button();
             button.Text = "Click Me";
             button.Click += new EventHandler(OnClick);
 
-            panel.Controls.Add(button);
+            panelTwo.Controls.Add(button);
 
 
-            this.Controls.Add(panel);
+            this.Controls.Add(panelOne);
+            this.Controls.Add(panelTwo);
+
+            ControlList = new List<Control>();
+            ListControls(this.Controls);
 
         }
 
         public void OnClick(Object sender, EventArgs args)
         {
 
-            MessageBox.Show("textbox.Text");
+            MessageBox.Show(this.textbox.Text);
+
+        }
+
+        public void ListControls(Control controls)
+        {
+            Console.WriteLine("Controls");
+
+            foreach (Control c in container.Controls)
+            {
+                ListControls(c);
+                this.ControlList.Add(c);
+            }
 
         }
 
