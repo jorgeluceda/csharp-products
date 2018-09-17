5e09ff8ae8e43976d1882579c8fd8e950ec573bc
Lazaro Alvarez <lalvarezcu@yahoo.es>
2018-09-16 22:40:22 -0700

    PreferencesDialog Added
    Added preferencesDialog
    Added background image to dialog's resource
    Added buttons/labels/textbox/panel
    AutoValidate = EnableAllowFocusChange
    Form resize allowed. Set min size
    Centered on resize set

---
+++ PreferencesDialog.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CoreLibrary;
+
+namespace Application
+{
+    public partial class PreferencesDialog : BaseForm
+    {
+        public PreferencesDialog()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void PreferencesDialog_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void preferencesOkButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void preferencesApplyButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void preferencesCancelButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
