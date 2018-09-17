2ea217097b99cd01985647930fa643fa15a4167e
Emmanuel500 <32080181+Emmanuel500@users.noreply.github.com>
2018-09-17 15:33:08 -0400

    Add files via upload
    Working Dialog Box

---
+++ CoreLibrary/BaseForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoreLibrary
+{
+    public partial class BaseForm : Form
+    {
+        public BaseForm()
+        {
+            InitializeComponent();
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void colorsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (BackgroundColor.ShowDialog() == DialogResult.OK)
+            {
+                this.BackColor = BackgroundColor.Color;
+            }
+        }
+    }
+}
