4939a15f9e739725ec8e41912d098fbdecc8fb56
Lazaro Alvarez <lalvarezcu@yahoo.es>
2018-09-16 23:33:40 -0700

    Validation Added
    Added validation function
    Added through validation for width, height, ratio
    Added error provider

--- PreferencesDialog.cs
+++ PreferencesDialog.cs
@@ -28,15 +28,62 @@ namespace Application
         private void preferencesOkButton_Click(object sender, EventArgs e)
         {
 
+            if (validateForm())
+            {
+                Console.Write("Process result");
+            }
+
         }
 
         private void preferencesApplyButton_Click(object sender, EventArgs e)
         {
 
+            if (validateForm())
+            {
+                Console.Write("Process result");
+            }
+
         }
 
         private void preferencesCancelButton_Click(object sender, EventArgs e)
         {
+
+        }
+
+        public bool validateForm()
+        {
+
+            String width = this.preferencesWidthTextBox.Text;
+
+            if (width == null || width.Length == 0 || !width.All(char.IsDigit) || 
+                Int32.Parse(width) < 10 || Int32.Parse(width) > 500)
+            {
+                this.preferencesErrorProvider.SetError(preferencesWidthTextBox, "Invalid Width");
+                return false;
+
+            }
+
+            String height = this.preferencesHeightTextBox.Text;
+
+            if (height == null || height.Length == 0 || !height.All(char.IsDigit) ||
+                Int32.Parse(height) < 10 || Int32.Parse(height) > 500)
+            {
+                this.preferencesErrorProvider.SetError(preferencesHeightTextBox, "Invalid Width");
+                return false;
+
+            }
+
+            String ratio = this.preferencesRatioTextBox.Text;
+
+            if (ratio == null || ratio.Length == 0 || !ratio.All(char.IsDigit) ||
+                Int32.Parse(ratio) < 10 || Int32.Parse(ratio) > 500)
+            {
+                this.preferencesErrorProvider.SetError(preferencesRatioTextBox, "Invalid Width");
+                return false;
+
+            }
+
+            return true;
 
         }
     }
