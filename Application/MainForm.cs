bc2ff7f375539910de54863f3c3c155b0dc98da0
Jorge Euceda <jeuce004@fiu.edu>
2018-09-18 20:21:40 -0400

    Darien module 3 main form v2 (#42)
    * changed color on preferences dialog
    
    * Started MainForm. Started Interface. Fixed validation error messages for different cases. Fixed BaseForm drag and move.
    
    * Updated interface property names (conflicted with built in .NET properties), implemented opening preferences dialog modelessly, finished Preferences dialog (the properties are now set properly)
    
    * moved openPreferencesModally logic to a context menu (it was in the top level menu), added ability to prevent closing main app, added close app tool strip item to top menu

--- Application/MainForm.cs
+++ Application/MainForm.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -23,24 +23,52 @@ namespace Application
             InitializeComponent();
         }
 
-        private void ellipseToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            //Ellipse ellipse = new Ellipse();
-        }
-
-        private void openModelesslyToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            PreferencesDialog dlg = new PreferencesDialog();
-            dlg.Apply += preferences_Apply;
-            dlg.Show();
-        }
-
         void preferences_Apply(object sender, EventArgs e)
         {
             ITeam3Interface preferencesDlg = sender as ITeam3Interface;
             this.ShapeWidth = preferencesDlg.ShapeWidth;
             this.ShapeHeight = preferencesDlg.ShapeHeight;
             this.ShapeRatio = preferencesDlg.ShapeRatio;
+        }
+
+        private void ellipseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Ellipse ellipse = new Ellipse(this.ShapeRatio, this.ShapeWidth);
+            ellipse.Show();
+        }
+
+        private void rectangleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Rectangular rectangle = new Rectangular(this.ShapeRatio, this.ShapeHeight);
+            rectangle.Show();
+        }
+
+        private void closeRectanglesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to exit the application?", 
+                "Closing Application", MessageBoxButtons.YesNo);
+
+            if(result == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void closeApplicationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void openPreferencesModallyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PreferencesDialog dlg = new PreferencesDialog();
+            dlg.Apply += preferences_Apply;
+            dlg.Show();
         }
     }
 }
