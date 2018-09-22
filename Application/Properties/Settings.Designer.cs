12221590b9c31d7e7c6f72a50e73802d6b130cca
Alejandro Perez <apere932@fiu.edu>
2018-09-22 15:19:32 -0400

    Added property store the show login functionality

--- Application/Properties/Settings.Designer.cs
+++ Application/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Application.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -131,6 +131,18 @@ namespace Application.Properties {
             }
             set {
                 this["Setting"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool ShowLogin {
+            get {
+                return ((bool)(this["ShowLogin"]));
+            }
+            set {
+                this["ShowLogin"] = value;
             }
         }
     }
