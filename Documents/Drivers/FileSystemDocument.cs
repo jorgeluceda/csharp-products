669edbbc144ab5f44142a2927c36b707ae1c2e79
Alejandro Perez <apere932@fiu.edu>
2018-09-29 10:44:33 -0400

    Working on the file system document driver

---
+++ Documents/Drivers/FileSystemDocument.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+
+namespace SingleDocumentInterface.Documents.Drivers
+{
+    [Serializable]
+    class FileSystemDocument : IDocument
+    {
+        #region BackColor
+
+        private int argbColor = 0;
+
+        public Color BackColor
+        {
+            get
+            {
+                if (this.argbColor == 0)
+                    return Color.Black;
+
+                return Color.FromArgb(this.argbColor);
+            }
+            set
+            {
+                this.argbColor = value.ToArgb();
+            }
+        }
+
+        #endregion
+
+        public string DocumentTitle { get; set; }
+        public string DocumentLocation { get; set; }
+        public Font Font { get; set; }
+        public float FontSize { get; set; }
+        public Color FontColor { get; set; }
+        public string Text { get; set; }
+    }
+}
--- Documents/IDocument.cs
+++ Documents/IDocument.cs
@@ -1,20 +1,15 @@
-﻿using System.IO;
-using System.Drawing;
-using System;
+﻿using System.Drawing;
 
 namespace SingleDocumentInterface.Documents
 {
     public interface IDocument
     {
         Color BackColor { get; set; }
-        Path DocumentLocation { get; set; }
+        string DocumentLocation { get; set; }
         string DocumentTitle { get; set; }
         Font Font { get; set; }
         float FontSize { get; set; }
         Color FontColor { get; set; }
-        int FormLocationX { get; set; }
-        int FormLocationY { get; set; }
-        bool Save();
         string Text { get; set; }
     }
 }
