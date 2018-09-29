99cf89f82b057943e875e82540a1d01188bc651a
Alejandro Perez <apere932@fiu.edu>
2018-09-29 14:37:45 -0400

    Added a README file

--- Program.cs
+++ Program.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml.Serialization;
 
 namespace SingleDocumentInterface
 {
