5633a8ff3223921c503b5a996b85148d5e1577bb
Emmanuel500 <32080181+Emmanuel500@users.noreply.github.com>
2018-10-09 19:31:21 -0400

    Add files via upload
    Made the OathDialog re-sizable and used the path gradient brush do design a background for the form. In the AboutDialog, I made it re-sizable also, changed the description to fit this module 6, and started working on making the pen for the borders (It is currently filling the entire background, but it was to check the pen was working).

--- AboutDialog.cs
+++ AboutDialog.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,19 @@ namespace SingleDocumentInterface
         public AboutDialog()
         {
             InitializeComponent();
+        }
+
+        //Method to change the border rectangle of the client
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            //Creating the LinearGradientBrush to be use
+            ColorBlend blend = new ColorBlend();
+            blend.Colors = new Color[] { Color.OrangeRed, Color.SkyBlue, Color.SeaGreen };
+            blend.Positions = new float[] { 0.0f, 0.5f, 1.0f };
+            LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle, Color.OrangeRed, Color.SeaGreen, LinearGradientMode.Horizontal);
+            brush.InterpolationColors = blend;
+            Pen pen = new Pen(brush);
+            e.Graphics.FillRectangle(brush, this.ClientRectangle);
         }
     }
 }
--- OathDialog.cs
+++ OathDialog.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +19,28 @@ namespace SingleDocumentInterface
         public OathDialog()
         {
             InitializeComponent();
+        }
+
+        //Method to change the backgorund of the form
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            //Creating the diamond points to be use by the brush
+            Point[] diamondPoints = new Point[] { new Point(Width / 16, 0),
+                                              new Point(0, Height / 12),
+                                              new Point (Width / 16, Height / 6),
+                                              new Point (Width / 8, Height / 12)};
+            //Creating the PathGradientBrush to fill the background color of the Oath Dialog
+            using (PathGradientBrush brush = new PathGradientBrush(diamondPoints))
+            {
+                Color[] colors = { Color.Blue, Color.Purple, Color.Red };
+                brush.CenterColor = Color.Azure;
+                brush.SurroundColors = colors;
+                brush.WrapMode = WrapMode.TileFlipXY;
+                //So the background of the gradient would not be black
+                e.Graphics.FillRectangle(new SolidBrush(Color.Orange), this.ClientRectangle);
+                //Filling the client area with the PathGradientBrush
+                e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            }
         }
     }
 }
