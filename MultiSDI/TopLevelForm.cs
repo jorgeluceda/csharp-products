ca7ae36d49dab323ebd68f644bb70b6494ec2639
Darien Morrison <dmorr041@fiu.edu>
2018-10-11 13:20:19 -0400

    Added custom Application context (MultiSDIApplication.cs), CreateTopLevelWindow method of TopLevelForm, as well as fileName property and File->Open click handler. These won't work until OpenFile is implemented properly.

--- MultiSDI/TopLevelForm.cs
+++ MultiSDI/TopLevelForm.cs
@@ -7,20 +7,87 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace MultiSDI
 {
     public partial class TopLevelForm : Form
     {
+        string fileName;   
+        
+        // Read Only FileName property
+        string FileName
+        {
+            get { return this.fileName; }
+        }
+
         public TopLevelForm()
         {
             InitializeComponent();
+        }
+
+        /**
+         *  CreateTopLevelWindow implements creating a new form. This is the real 'constructor'. 
+         *  First, we check if the fileName matches any of the Open Forms in the MultiSDIApplication. If it
+         *  does, we just activate that form. If it doesn't we create a new one using OpenFile, a method found
+         *  in this class (TopLevelForm) to open a file, then we activate the new window.
+         */
+        public static TopLevelForm CreateTopLevelWindow(string fileName)
+        {
+            // If fileName is not empty
+            if (!string.IsNullOrEmpty(fileName))
+            {   
+                // Loop through the open forms in MultiSDIApplication 
+                foreach(TopLevelForm openForm in Application.OpenForms)
+                {
+                    // If the file we're trying to open is already open, i.e. file names match
+                    if(string.Compare(openForm.fileName, fileName, true) == 0)
+                    {
+                        // Bring form to top
+                        openForm.Activate();
+                        return openForm;
+                    }
+                }
+            }
+
+            // Otherwise, create a new one with the given fileName
+            TopLevelForm form = new TopLevelForm();
+            form.OpenFile(fileName);
+            form.Show();
+
+            // Bring form to top
+            form.Activate();
+            return form;
+        }
+
+        /**
+         *  OpenFile opens a file given it's fileName. Sets this top level form's file name to the fileName passed
+         *  in the call, and then populates this form with the contents of the document.
+         */
+        void OpenFile(string fileName)
+        {
+            // TODO: Implement populating the TopLevelForm with the shapes from the saved document. This part will differ
+            // from the book, which reads text into the top level forms textbox. We, however, are not dealing with text, but
+            //  with shapes
         }
 
         private void shapeOptionsToolStripMenuItem_Click(object sender, EventArgs e)
         {
             ShapeOptions dlg = new ShapeOptions();
             dlg.Show();
+        }
+
+        /**
+         * Click handler for File->Open. Opens a file using an OpenFileDialog, passing the fileName to 
+         * CreateTopLevelWindow.
+         */
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if(dlg.ShowDialog() == DialogResult.OK)
+            {
+                CreateTopLevelWindow(dlg.FileName);
+            }
         }
     }
 }
