c0c44b60714688b9936c7caa6bea9325121175f3
Darien Morrison <dmorr041@fiu.edu>
2018-10-10 20:02:24 -0400

    Fixed MultiSDI, should show up now...

---
+++ MultiSDI/MainForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiSDI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void shapeOptionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShapeOptions dlg = new ShapeOptions();
+            dlg.Show();
+        }
+    }
+}
