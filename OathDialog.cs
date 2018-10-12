74aa4d1dfe8f494a6b6c7a32c3ab72bab229f44b
Emmanuel500 <32080181+Emmanuel500@users.noreply.github.com>
2018-10-12 11:47:18 -0400

    Add files via upload
    Oath Dialog and About Dialog finished and added for the new merge. Oath contains the diamond background with the PathGradientBrush while About contains the border Pen.

--- OathDialog.cs
+++ OathDialog.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using CoreLibrary;
-
+using System.Drawing.Drawing2D;
 
 
 namespace SingleDocumentInterface
@@ -18,6 +18,26 @@ namespace SingleDocumentInterface
         public OathDialog()
         {
             InitializeComponent();
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            //Creating the PathGradientBrush to fill the background color of the Oath Dialog
+            Point[] diamondPoints = new Point[] { new Point(Width / 16, 0),
+                                              new Point(0, Height / 12),
+                                              new Point (Width / 16, Height / 6),
+                                              new Point (Width / 8, Height / 12)};
+            using (PathGradientBrush brush = new PathGradientBrush(diamondPoints))
+            {
+                Color[] colors = { Color.Blue, Color.Purple, Color.Red };
+                brush.CenterColor = Color.Azure;
+                brush.SurroundColors = colors;
+                brush.WrapMode = WrapMode.TileFlipXY;
+                //So the background of the gradient would not be black
+                e.Graphics.FillRectangle(new SolidBrush(Color.Orange), this.ClientRectangle);
+                //Filling the client area with the PathGradientBrush
+                e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            }
         }
     }
 }
