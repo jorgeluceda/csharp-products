6ef4b08c2de2dfbe36b858e875410e0406c82125
Emmanuel500 <32080181+Emmanuel500@users.noreply.github.com>
2018-10-19 20:58:43 -0400

    Add files via upload
    Created the About and Oath Dialog for MultiSDIText.

---
+++ MultiSDIText/OathDialog.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace TextMDI
+{
+    partial class oathDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.oathControl = new CoreLibrary.oathControl();
+            this.middlePanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // middlePanel
+            // 
+            this.middlePanel.BackColor = System.Drawing.Color.Transparent;
+            this.middlePanel.Controls.Add(this.oathControl);
+            this.middlePanel.Size = new System.Drawing.Size(457, 253);
+            // 
+            // oathControl
+            // 
+            this.oathControl.BackColor = System.Drawing.Color.Transparent;
+            this.oathControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.oathControl.Location = new System.Drawing.Point(0, 0);
+            this.oathControl.Name = "oathControl";
+            this.oathControl.Size = new System.Drawing.Size(457, 253);
+            this.oathControl.TabIndex = 0;
+            // 
+            // oathDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 363);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "oathDialog";
+            this.ShowIcon = false;
+            this.Text = "Oath DIalog";
+            this.middlePanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CoreLibrary.oathControl oathControl;
+    }
+}
---
+++ MultiSDIText/OathDialog.cs
@@ -0,0 +1,44 @@
+﻿using CoreLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextMDI
+{
+    public partial class oathDialog : BaseDialogForm
+    {
+        public oathDialog()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            //Creating the PathGradientBrush to fill the background color of the Oath Dialog
+            Point[] diamondPoints = new Point[] {   new Point(30,0),
+                                                    new Point(60,60),
+                                                    new Point(0,20),
+                                                    new Point(60,20),
+                                                    new Point(0,60),
+                                                    new Point(30,0)};
+            using (PathGradientBrush brush = new PathGradientBrush(diamondPoints))
+            {
+                Color[] colors = { Color.LightSteelBlue, Color.SkyBlue };
+                brush.CenterColor = Color.LightSlateGray;
+                brush.SurroundColors = colors;
+                brush.WrapMode = WrapMode.Tile;
+                //So the background of the gradient would not be black
+                e.Graphics.FillRectangle(new SolidBrush(Color.PaleGoldenrod), this.ClientRectangle);
+                //Filling the client area with the PathGradientBrush
+                e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            }
+        }
+    }
+}
