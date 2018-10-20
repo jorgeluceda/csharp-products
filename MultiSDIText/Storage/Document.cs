07c9928d226882ab1c717a684dac52e9b9dbc9a0
Alejandro Perez <apere932@fiu.edu>
2018-10-20 12:44:24 -0400

    Fixed error with namespace

---
+++ MultiSDIText/Storage/Document.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiSDIText.Storage
+{
+    public class Document
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Simple constructor that initilaizes all properties
+        /// </summary>
+        public Document()
+        {
+            this.Content = new Container();
+        }
+
+        #endregion
+
+        #region Content
+
+        /// <summary>
+        /// Holds a list of all the text objects
+        /// </summary>
+        private Container Content { get; set; }
+
+        #endregion
+
+        #region Functionality
+
+        // Region where all the documents actions are going to be performed
+
+        #endregion
+    }
+}
