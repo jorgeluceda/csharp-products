4c96a6a404c570935f3e444fc472f1c0d32dcacc
Alejandro Perez <apere932@fiu.edu>
2018-10-22 17:09:22 -0400

    Update MultiSDIText/Storage/Document.cs

--- MultiSDIText/Storage/Document.cs
+++ MultiSDIText/Storage/Document.cs
@@ -37,9 +37,46 @@ namespace MultiSDIText.Storage
         /// </summary>
         private List<Text> content;
 
+        /// <summary>
+        /// Helper method to add a text to the content, in a sorted manner
+        /// </summary>
+        /// <param name="text">The text to be added</param>
         public void Add(Text text)
         {
             this.content.Add(text);
+
+            if (this.content.Count > 0)
+                this.SortContentByZOrder();
+        }
+
+        /// <summary>
+        /// Remove the first occurrence of a text
+        /// </summary>
+        /// <param name="text">The text to be removed</param>
+        /// <returns>Whether or not the text was removed successfully</returns>
+        public bool Delete(Text text)
+        {
+            return this.content.Remove(text);
+        }
+
+        /// <summary>
+        /// Remove element at index specified
+        /// </summary>
+        /// <param name="index">The index to be removed</param>
+        public void DeleteAt(int index)
+        {
+            this.content.RemoveAt(index);
+        }
+
+        /// <summary>
+        /// Helper method to sort the content by ZOrder
+        /// </summary>
+        private void SortContentByZOrder()
+        {
+            this.content.Sort(delegate (Text x, Text y)
+            {
+                return y.ZOrder.CompareTo(x.ZOrder);
+            });
         }
 
         #endregion
