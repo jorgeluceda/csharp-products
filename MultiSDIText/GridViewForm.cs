91b9bbd8ac773187fa5902f87a2c7de91b4215da
Alejandro Perez <apere932@fiu.edu>
2018-11-02 20:02:22 -0400

    Added binding source to the grid view form

--- MultiSDIText/GridViewForm.cs
+++ MultiSDIText/GridViewForm.cs
@@ -10,11 +10,26 @@ using System.Windows.Forms;
 
 namespace MultiSDIText
 {
-    public partial class GridViewForm : Form
+    public partial class GridViewForm : Form, IBindingSource
     {
         public GridViewForm()
         {
             InitializeComponent();
+        }
+
+        public BindingSource DataBindingSource
+        {
+            get { return this.documentBindingSource; }
+            set { this.documentBindingSource = value; }
+        }
+
+        BindingManagerBase BindingManager
+        {
+
+            get
+            {
+                return this.BindingContext[this.documentBindingSource];
+            }
         }
     }
 }
