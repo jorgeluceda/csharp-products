3edcf3c7f7af5b5a18f314f4ca537ac556816aa5
lalvarezcu <lalvarezcu@yahoo.es>
2018-11-04 07:11:04 -0800

    added Key State Enum

--- MultiSDIText/GridViewForm.cs
+++ MultiSDIText/GridViewForm.cs
@@ -13,6 +13,18 @@ namespace MultiSDIText
 {
     public partial class GridViewForm : Form, IBindingSource
     {
+        // KeyState
+        [FlagsAttribute]
+        enum KeyState
+        {
+            LeftMouse = 1,
+            RightMouse = 2,
+            ShiftKey = 4,
+            CtrlKey = 8,
+            MiddleMouse = 16,
+            AltKey = 32,
+        }
+
         public GridViewForm()
         {
             InitializeComponent();
