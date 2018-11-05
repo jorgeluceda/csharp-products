e4708fdc41327722b1293dfcd7b3a95082e61d83
Mohammed Bhatti <mbhatti125@gmail.com>
2018-11-05 16:30:21 -0500

    Animated cursor now stays on menu bar

--- MultiSDIText/TopLevelForm.cs
+++ MultiSDIText/TopLevelForm.cs
@@ -642,5 +642,10 @@ namespace MultiSDIText
 
         }
 
+        private void MainMenu_MouseEnter(object sender, EventArgs e)
+        {
+            //Change cursor to the animated cursor
+            this.Cursor = AnimatedCursor;
+        }
     }
 }
