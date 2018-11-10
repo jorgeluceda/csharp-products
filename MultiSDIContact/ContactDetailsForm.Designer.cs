1a371489db8dad2f0598bc62a52e706873fed783
Emmanuel Perez Hernandez <epere328@fiu.edu>
2018-11-09 20:13:47 -0500

    Inserted buttons and labels for Contact Details Form

--- MultiSDIContact/ContactDetailsForm.Designer.cs
+++ MultiSDIContact/ContactDetailsForm.Designer.cs
@@ -28,13 +28,246 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.homePhoneLabel = new System.Windows.Forms.Label();
+            this.homeTextBox = new System.Windows.Forms.TextBox();
+            this.cellPhoneLabel = new System.Windows.Forms.Label();
+            this.cellPhoneTextBox = new System.Windows.Forms.TextBox();
+            this.address1Label = new System.Windows.Forms.Label();
+            this.address1TextBox = new System.Windows.Forms.TextBox();
+            this.address2Label = new System.Windows.Forms.Label();
+            this.address2TextBox = new System.Windows.Forms.TextBox();
+            this.cityLabel = new System.Windows.Forms.Label();
+            this.cityTextBox = new System.Windows.Forms.TextBox();
+            this.stateTextBox = new System.Windows.Forms.TextBox();
+            this.stateLabel = new System.Windows.Forms.Label();
+            this.zipLabel = new System.Windows.Forms.Label();
+            this.zipTextBox = new System.Windows.Forms.TextBox();
+            this.countryLabel = new System.Windows.Forms.Label();
+            this.countryTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(12, 489);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 0;
+            this.okButton.Text = "Ok";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(109, 489);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(298, 489);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 2;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Location = new System.Drawing.Point(204, 35);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(80, 17);
+            this.firstNameLabel.TabIndex = 3;
+            this.firstNameLabel.Text = "First Name:";
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(207, 55);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(157, 22);
+            this.firstNameTextBox.TabIndex = 4;
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(204, 107);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(80, 17);
+            this.lastNameLabel.TabIndex = 5;
+            this.lastNameLabel.Text = "Last Name:";
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(207, 127);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(157, 22);
+            this.lastNameTextBox.TabIndex = 6;
+            // 
+            // homePhoneLabel
+            // 
+            this.homePhoneLabel.AutoSize = true;
+            this.homePhoneLabel.Location = new System.Drawing.Point(203, 186);
+            this.homePhoneLabel.Name = "homePhoneLabel";
+            this.homePhoneLabel.Size = new System.Drawing.Size(94, 17);
+            this.homePhoneLabel.TabIndex = 7;
+            this.homePhoneLabel.Text = "Home Phone:";
+            // 
+            // homeTextBox
+            // 
+            this.homeTextBox.Location = new System.Drawing.Point(206, 210);
+            this.homeTextBox.Name = "homeTextBox";
+            this.homeTextBox.Size = new System.Drawing.Size(157, 22);
+            this.homeTextBox.TabIndex = 8;
+            // 
+            // cellPhoneLabel
+            // 
+            this.cellPhoneLabel.AutoSize = true;
+            this.cellPhoneLabel.Location = new System.Drawing.Point(11, 186);
+            this.cellPhoneLabel.Name = "cellPhoneLabel";
+            this.cellPhoneLabel.Size = new System.Drawing.Size(80, 17);
+            this.cellPhoneLabel.TabIndex = 9;
+            this.cellPhoneLabel.Text = "Cell Phone:";
+            // 
+            // cellPhoneTextBox
+            // 
+            this.cellPhoneTextBox.Location = new System.Drawing.Point(11, 210);
+            this.cellPhoneTextBox.Name = "cellPhoneTextBox";
+            this.cellPhoneTextBox.Size = new System.Drawing.Size(157, 22);
+            this.cellPhoneTextBox.TabIndex = 10;
+            // 
+            // address1Label
+            // 
+            this.address1Label.AutoSize = true;
+            this.address1Label.Location = new System.Drawing.Point(11, 254);
+            this.address1Label.Name = "address1Label";
+            this.address1Label.Size = new System.Drawing.Size(76, 17);
+            this.address1Label.TabIndex = 11;
+            this.address1Label.Text = "Address 1:";
+            // 
+            // address1TextBox
+            // 
+            this.address1TextBox.Location = new System.Drawing.Point(14, 275);
+            this.address1TextBox.Name = "address1TextBox";
+            this.address1TextBox.Size = new System.Drawing.Size(349, 22);
+            this.address1TextBox.TabIndex = 12;
+            // 
+            // address2Label
+            // 
+            this.address2Label.AutoSize = true;
+            this.address2Label.Location = new System.Drawing.Point(14, 313);
+            this.address2Label.Name = "address2Label";
+            this.address2Label.Size = new System.Drawing.Size(76, 17);
+            this.address2Label.TabIndex = 13;
+            this.address2Label.Text = "Address 2:";
+            // 
+            // address2TextBox
+            // 
+            this.address2TextBox.Location = new System.Drawing.Point(14, 334);
+            this.address2TextBox.Name = "address2TextBox";
+            this.address2TextBox.Size = new System.Drawing.Size(349, 22);
+            this.address2TextBox.TabIndex = 14;
+            // 
+            // cityLabel
+            // 
+            this.cityLabel.AutoSize = true;
+            this.cityLabel.Location = new System.Drawing.Point(14, 371);
+            this.cityLabel.Name = "cityLabel";
+            this.cityLabel.Size = new System.Drawing.Size(35, 17);
+            this.cityLabel.TabIndex = 15;
+            this.cityLabel.Text = "City:";
+            // 
+            // cityTextBox
+            // 
+            this.cityTextBox.Location = new System.Drawing.Point(14, 392);
+            this.cityTextBox.Name = "cityTextBox";
+            this.cityTextBox.Size = new System.Drawing.Size(134, 22);
+            this.cityTextBox.TabIndex = 16;
+            // 
+            // stateTextBox
+            // 
+            this.stateTextBox.Location = new System.Drawing.Point(165, 392);
+            this.stateTextBox.Name = "stateTextBox";
+            this.stateTextBox.Size = new System.Drawing.Size(53, 22);
+            this.stateTextBox.TabIndex = 17;
+            // 
+            // stateLabel
+            // 
+            this.stateLabel.AutoSize = true;
+            this.stateLabel.Location = new System.Drawing.Point(162, 371);
+            this.stateLabel.Name = "stateLabel";
+            this.stateLabel.Size = new System.Drawing.Size(45, 17);
+            this.stateLabel.TabIndex = 18;
+            this.stateLabel.Text = "State:";
+            // 
+            // zipLabel
+            // 
+            this.zipLabel.AutoSize = true;
+            this.zipLabel.Location = new System.Drawing.Point(236, 372);
+            this.zipLabel.Name = "zipLabel";
+            this.zipLabel.Size = new System.Drawing.Size(32, 17);
+            this.zipLabel.TabIndex = 19;
+            this.zipLabel.Text = "Zip:";
+            // 
+            // zipTextBox
+            // 
+            this.zipTextBox.Location = new System.Drawing.Point(239, 392);
+            this.zipTextBox.Name = "zipTextBox";
+            this.zipTextBox.Size = new System.Drawing.Size(125, 22);
+            this.zipTextBox.TabIndex = 20;
+            // 
+            // countryLabel
+            // 
+            this.countryLabel.AutoSize = true;
+            this.countryLabel.Location = new System.Drawing.Point(14, 423);
+            this.countryLabel.Name = "countryLabel";
+            this.countryLabel.Size = new System.Drawing.Size(57, 17);
+            this.countryLabel.TabIndex = 21;
+            this.countryLabel.Text = "Country";
+            // 
+            // countryTextBox
+            // 
+            this.countryTextBox.Location = new System.Drawing.Point(14, 444);
+            this.countryTextBox.Name = "countryTextBox";
+            this.countryTextBox.Size = new System.Drawing.Size(349, 22);
+            this.countryTextBox.TabIndex = 22;
             // 
             // ContactDetailsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(516, 524);
+            this.ClientSize = new System.Drawing.Size(385, 524);
+            this.Controls.Add(this.countryTextBox);
+            this.Controls.Add(this.countryLabel);
+            this.Controls.Add(this.zipTextBox);
+            this.Controls.Add(this.zipLabel);
+            this.Controls.Add(this.stateLabel);
+            this.Controls.Add(this.stateTextBox);
+            this.Controls.Add(this.cityTextBox);
+            this.Controls.Add(this.cityLabel);
+            this.Controls.Add(this.address2TextBox);
+            this.Controls.Add(this.address2Label);
+            this.Controls.Add(this.address1TextBox);
+            this.Controls.Add(this.address1Label);
+            this.Controls.Add(this.cellPhoneTextBox);
+            this.Controls.Add(this.cellPhoneLabel);
+            this.Controls.Add(this.homeTextBox);
+            this.Controls.Add(this.homePhoneLabel);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.firstNameLabel);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
             this.HelpButton = true;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -42,9 +275,34 @@
             this.ShowIcon = false;
             this.Text = "Contact Details";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.Label homePhoneLabel;
+        private System.Windows.Forms.TextBox homeTextBox;
+        private System.Windows.Forms.Label cellPhoneLabel;
+        private System.Windows.Forms.TextBox cellPhoneTextBox;
+        private System.Windows.Forms.Label address1Label;
+        private System.Windows.Forms.TextBox address1TextBox;
+        private System.Windows.Forms.Label address2Label;
+        private System.Windows.Forms.TextBox address2TextBox;
+        private System.Windows.Forms.Label cityLabel;
+        private System.Windows.Forms.TextBox cityTextBox;
+        private System.Windows.Forms.TextBox stateTextBox;
+        private System.Windows.Forms.Label stateLabel;
+        private System.Windows.Forms.Label zipLabel;
+        private System.Windows.Forms.TextBox zipTextBox;
+        private System.Windows.Forms.Label countryLabel;
+        private System.Windows.Forms.TextBox countryTextBox;
     }
 }
--- MultiSDIContact/ContactDetailsForm.cs
+++ MultiSDIContact/ContactDetailsForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,34 @@ namespace MultiSDIContact
 {
     public partial class ContactDetailsForm : Form
     {
+        #region Variables
+
+        #endregion
+
+        #region Contructor
         public ContactDetailsForm()
         {
             InitializeComponent();
         }
+        #endregion
+
+        #region Drawing Background
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            //Creating the LinearGradientBrush to be use
+            using (LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle, Color.OrangeRed, Color.SeaGreen, LinearGradientMode.ForwardDiagonal))
+            {
+                ColorBlend blend = new ColorBlend();
+                blend.Colors = new Color[] { Color.Red, Color.Orange, Color.Yellow };
+                blend.Positions = new float[] { 0.0f, 0.5f, 1.0f };
+                brush.InterpolationColors = blend;
+                Pen pen = new Pen(brush, 30.0f);
+                e.Graphics.FillRectangle(new SolidBrush(Color.LightSteelBlue), this.ClientRectangle);
+                e.Graphics.DrawRectangle(pen, this.ClientRectangle);
+            }
+        }
+        #endregion
+
     }
 }
