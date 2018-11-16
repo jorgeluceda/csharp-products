bc81fec7e28622a331e14b0b0e63f770053e9a23
Alejandro Perez <apere932@fiu.edu>
2018-11-16 18:49:30 -0500

    Added bindiable functionality to the base entity

--- MultiSDIContact/Services/Entities/BaseEntity.cs
+++ MultiSDIContact/Services/Entities/BaseEntity.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MultiSDIContact.Services.Entities
 {
-    public class BaseEntity
+    public class BaseEntity : INotifyPropertyChanged
     {
+        #region DataBinding
+
+        /// <summary>
+        /// The property change handler
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Fires the property change event for a property
+        /// </summary>
+        /// <param name="propertyName">The property name that changed</param>
+        protected void PropertyChange(String propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
     }
 }
